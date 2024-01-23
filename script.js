1cce0b458e995a58477e181a08e1868ce09c5c7c
boel78 <95147718+boel78@users.noreply.github.com>
2024-01-23 12:44:08 +0100

    uppdaterar displayen när man klickar i nummer

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+var firstNumber;
+var operator;
+var secondNumber;
+let displayValue = "";
+
+const btns = document.querySelectorAll('button');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let btnValue = btn.getAttribute('id');
+        console.log(btnValue);
+        if(Number.isInteger(parseInt(btnValue))){
+            displayValue += btnValue;
+            document.querySelector("#display").textContent = displayValue;
+        }
+    });
+});
+
+
+function operate(first, second, op){
+    switch(op){
+        case "+":
+            add(first, second);
+        break;
+        case "-":
+            subtract(first, second);
+        break;
+        case "*":
+            multiply(first, second);
+        break;
+        case "/":
+            divide(first, second);
+        break;
+    }
+}
+
 function add(x, y){
     return x+y;
 }
