3a61c21667b72b612a8b8fad06a621f8c4a2ca83
boel78 <95147718+boel78@users.noreply.github.com>
2024-01-23 14:31:34 +0100

    fixed design

--- script.js
+++ script.js
@@ -10,82 +10,107 @@ btns.forEach(btn => {
     btn.addEventListener('click', () => {
         let btnValue = btn.getAttribute('id');
         console.log(btnValue);
-        if(Number.isInteger(parseInt(btnValue))){
-            displayValue += btnValue;
-            update()
-        }
-        else if(btnValue == "add"){
-            console.log("addition")
-            setFirstValue(displayValue);
-            displayValue = "";
-            document.querySelector("#display").textContent = "";
-            selectedOp = "+";
-        }
-        else if(btnValue == "equals"){
-            setSecondValue(displayValue);
-            operate(firstNumber, secondNumber, selectedOp)
-        }
-        else if(btnValue == "clear"){
-            displayValue = "";
-            firstNumber = "";
-            secondNumber = "";
-            selectedOp = "";
-            update()
+
+        switch (btnValue) {
+            case "add":
+                console.log("addition")
+                setFirstValue(displayValue);
+                displayValue = "";
+                document.querySelector("#display").textContent = "";
+                selectedOp = "+";
+                break;
+            case "equals":
+                setSecondValue(displayValue);
+                if(firstNumber != "" && secondNumber != ""){
+                    operate(firstNumber, secondNumber, selectedOp)
+                }
+                break;
+            case "clear":
+                displayValue = "";
+                firstNumber = "";
+                secondNumber = "";
+                selectedOp = "";
+                update()
+                break;
+            case "subtract":
+                setFirstValue(displayValue);
+                displayValue = "";
+                document.querySelector("#display").textContent = "";
+                selectedOp = "-";
+                break;
+            case "multiply":
+                setFirstValue(displayValue);
+                displayValue = "";
+                document.querySelector("#display").textContent = "";
+                selectedOp = "*";
+                break;
+            case "divide":
+                setFirstValue(displayValue);
+                displayValue = "";
+                document.querySelector("#display").textContent = "";
+                selectedOp = "/";
+                break;
+            default:
+                displayValue += btnValue;
+                update()
+                break;
         }
     });
 });
 
 
-function operate(firs, secon, op){
+function operate(firs, secon, op) {
     let first = parseInt(firs);
     let second = parseInt(secon)
     let sum = 0;
     console.log(op)
-    switch(op){
+    switch (op) {
         case "+":
             console.log("case addiion")
-            sum =add(first, second);
-        break;
+            sum = add(first, second);
+            break;
         case "-":
             sum = subtract(first, second);
-        break;
+            break;
         case "*":
             sum = multiply(first, second);
-        break;
+            break;
         case "/":
             sum = divide(first, second);
-        break;
-        
+            break;
+
     }
     console.log(sum)
     displayValue = sum;
+    firstNumber = "";
+    secondNumber = "";
     update()
 }
 
-function add(x, y){
-    return x+y;
+function add(x, y) {
+    return x + y;
 }
 
-function subtract(x, y){
-    return x-y;
+function subtract(x, y) {
+    return x - y;
 }
 
-function divide(x, y){
-    return x/y;
+function divide(x, y) {
+    return x / y;
 }
 
-function multiply(x, y){
-    return x*y;
+function multiply(x, y) {
+    return x * y;
 }
 
-function setFirstValue(number){
+function setFirstValue(number) {
     firstNumber = number;
 }
 
-function setSecondValue(number){
+function setSecondValue(number) {
     secondNumber = number
 }
 
-function update(){
+function update() {
     document.querySelector("#display").textContent = displayValue;
 }
